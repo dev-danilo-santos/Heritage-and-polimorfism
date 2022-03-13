3facc9c738a3592255629a1ac4240a87d1b4e2c5
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-03-13 13:52:39 -0300

    aula 01 Herança

---
+++ Heritage01/Entities/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage01.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; protected set; }
+        
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public Account()
+        {
+
+        }
+
+        public void Withdraw(double value) 
+        {
+            if (value <= Balance && value > 0) {
+                Balance -= value;
+                Console.WriteLine($"Withdray {value}");
+            }
+            else if (value < 0) { Console.WriteLine("The value it can not be negative! "); }
+            else if (value > Balance)
+            {
+                Console.WriteLine("The value is greater than avaiable! ");
+            }
+
+        }
+
+        public void Deposit(double value)
+        {
+            if (value < 0) { Balance += value; }
+            else { Console.WriteLine("The value must be greather than zero"); }
+        }
+
+    }
+}
---
+++ Heritage01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heritage01.Entities;
+
+namespace Heritage01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+        }
+    }
+}
